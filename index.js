1ab5c5dc3bfe2cab334dfa231ce7d756229d79cc
Veronica Miller <vmcmiller@gmail.com>
2019-02-10 22:08:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function logDriverNames(drivers) {
 function logDriversByHometown(drivers, hometown) {
     drivers.forEach(function(driver) {
       if(driver.hometown === hometown) {
-        console.log(`${driver.name}`);
-    })};
+        console.log(`${driver.name}`)};
+    )};
 };
